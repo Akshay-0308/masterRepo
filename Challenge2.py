1b068d847371d33892186dceaeff041ae60276f1
Akshay-0308 <109257650+Akshay-0308@users.noreply.github.com>
2022-07-16 13:33:27 +1000

    Add files via upload

---
+++ Challenge2.py
@@ -0,0 +1,37 @@
+class test1 {
+
+	static char getMaxOccurringChar(String str)
+	{		
+		int count[] = new int[256];
+			
+		int len = str.length();
+		for (int i=0; i<len; i++){
+			
+			count[str.charAt(i)]++;
+		}
+	
+		int max = -1; 
+		char result = ' ';
+	
+		for (int i = 0; i < len; i++) {
+			if (max < count[str.charAt(i)]) {
+				max = count[str.charAt(i)];
+				result = str.charAt(i);
+			}
+		}
+	
+		return result;
+	}
+	
+	public static void main(String[] args)
+	{
+		String str = "Character";
+		System.out.println("Max occurring character is " +getMaxOccurringChar(str));
+	}
+}
+}
+
+
+
+
+
